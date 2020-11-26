35c264aa1cff7ad64297637034828a9525f537ac
Andrew Wood <andrewwood90@hotmail.com>
2020-11-26 11:59:32 +0000

    five tests passed

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,10 +1,10 @@
 def fizz_buzz(number):
-    if number == 3:
+    if number == 15:
+        return "fizzbuzz"
+    elif number % 3 == 0:
         return "fizz"
     elif number == 5:
         return "buzz"
-    elif number  == 15:
-        return "fizzbuzz"
     else:
         return str(number)
     
